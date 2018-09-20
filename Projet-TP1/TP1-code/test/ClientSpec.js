3d3ea8f3a4adc6c558d0ecdffbb46b32ffcdb11d
jacob643 <jacobdorais25@hotmail.com>
2018-09-20 14:19:33 -0400

    add test client.uploadAttachment all good

--- Projet-TP1/TP1-code/test/ClientSpec.js
+++ Projet-TP1/TP1-code/test/ClientSpec.js
@@ -4,12 +4,42 @@ let assert = require('chai').assert;
 let expect = require('chai').expect;
 let sinon = require('sinon');
 
-export default describe('CLient', () => {
+export default describe('Client', () => {
   let client;
   let stubFetch;
+  let sharedbox;
   beforeEach(() => {
     client = new SharedBox.Client('api', 1, 'blah');
     stubFetch = sinon.stub(Utils, 'fetch').withArgs(sinon.match.string, sinon.match.object);
+    sharedbox = {
+      userEmail: 'user@acme.com',
+      guid: '1c820789a50747df8746aa5d71922a3f',
+      uploadUrl: 'upload_url',
+      recipients: [ /* list of Recipient objects*/ ],
+      attachments: [ /*list of Attachment objects*/ ],
+      message: 'lorem ipsum...',
+      subject: 'Donec rutrum congue leo eget malesuada.',
+      notificationLanguage: 'en',
+      securityOptions: {
+        allowRememberMe: true,
+        allowSms: true,
+        allowVoice: true,
+        allowEmail: true,
+        expirationValue: 5,
+        expirationUnit: 'days',
+        retentionPeriodType: 'do_not_discard',
+        retentionPeriodValue: null,
+        retentionPeriodUnit: 'hours',
+        allowManualClose: true
+      },
+      userId: 1,
+      status: 'in_progress',
+      previewUrl: 'http://sharedbox.com/sharedboxes/dhjewg67ewtfg476/preview',
+      createdAt: '2018-05-24T14:45:35.062Z',
+      updatedAt: '2018-05-24T14:45:35.589Z',
+      expiration: '2018-05-31T14:45:35.038Z',
+      closedAt: null
+    };
   });
 
   afterEach(() => {
@@ -54,8 +84,8 @@ export default describe('CLient', () => {
         },
         json: () => {
           return {
-            guid: 'the guid',
-            uploadUrl: 'the uploadUrl'
+            guid: 'dc6f21e0f02c41123b795e4',
+            uploadUrl: 'upload_url'
           };
         }
       });
@@ -63,8 +93,8 @@ export default describe('CLient', () => {
       return client.initializeSharedBox({
         userEmail: 'bb@b.ba'
       }).then(res => {
-        expect(res.guid).to.equals('the guid');
-        expect(res.uploadUrl).to.equals('the uploadUrl');
+        expect(res.guid).to.equals('dc6f21e0f02c41123b795e4');
+        expect(res.uploadUrl).to.equals('upload_url');
       }, () => {
         assert(false);
       });
@@ -76,10 +106,9 @@ export default describe('CLient', () => {
   describe('submitSharedBox', () => {
 
     it('should throw error if GUID is null or undefined', () => {
+      sharedbox.guid = null;
       try {
-        client.submitSharedBox({
-          guid: null
-        }).then(() => {
+        client.submitSharedBox(sharedbox).then(() => {
           assert(false);
         }, () => {
           assert(false);
@@ -99,12 +128,7 @@ export default describe('CLient', () => {
         ok: false,
         text: ''
       });
-      return client.submitSharedBox({
-        guid: 'not null',
-        JsonClient: 'jsonclient',
-        Client: 'theclient',
-        Helpers: 'helper'
-      }).then(() => {
+      return client.submitSharedBox(sharedbox).then(() => {
         assert(false);
       }, result => {
         expect(result).to.be.an('error');
@@ -130,17 +154,12 @@ export default describe('CLient', () => {
         },
         json: () => {
           return {
-            guid: 'the guid',
-            uploadUrl: 'the uploadUrl',
-            JsonClient: 'jsonclient',
-            Client: 'theclient',
-            Helpers: 'helper'
+            guid: 'dc6f21e0f02c41123b795e4',
+            uploadUrl: 'upload_url'
           };
         }
       });
-      return client.submitSharedBox({
-        guid: 'not null',
-      }).then(result => {
+      return client.submitSharedBox(sharedbox).then(result => {
         expect(result).to.be.an('Object').to.have.all.keys('expiration', 'guid',
           'message', 'notificationLanguage', 'securityOptions', 'subject',
           'uploadUrl', 'userEmail');
@@ -151,5 +170,40 @@ export default describe('CLient', () => {
     });
 
   });
+
+  describe('uploadAttachment', () => {
+
+    let attachment;
+    let stubupload;
+    beforeEach(() => {
+      attachment = {
+        stream: 'stream',
+        contentType: 'contenttype',
+        filename: 'filename',
+        guid: 'none'
+      };
+      stubupload = sinon.stub(client.jsonClient, 'uploadFile')
+        .withArgs(sinon.match.string, sinon.match.object);
+    });
+
+    it('should return an attachment with the same guid as the docment guid', () => {
+      let response = {
+        temporaryDocument: {
+          documentGuid: '65f53ec1282c454fa98439dbda134093'
+        }
+      };
+      stubupload.onCall(0).resolves(response);
+      return client.uploadAttachment(sharedbox, attachment).then(result => {
+        expect(result.guid).to.equals(response.temporaryDocument.documentGuid);
+      }, result => {
+        expect(result).not.to.be.an('error');
+      });
+
+    });
+
+  });
+
+
+
 
 });
