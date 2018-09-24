e26de1a7c091a095681fd04f216d08d426b34bdb
jacob643 <jacobdorais25@hotmail.com>
2018-09-24 10:17:10 -0400

    ajout mini tests cancereux

--- TP2/algs4-master/src/main/java/edu/Tp2q41.java
+++ TP2/algs4-master/src/main/java/edu/Tp2q41.java
@@ -2,18 +2,34 @@ package edu;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.Exception;
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.GraphGenerator;
+
 class Tp2q41 {
 
+	Graph graphe;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+	
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		graphe = GraphGenerator.simple(3, 2);
+		//assertThrows(IllegalArgumentException, GraphGenerator.simple(2, 5));
+		assertEquals(0,0, "ca devrais generer un graphe simple");
+		
+		fail("pas fini dimplementer");
 	}
 
 }
