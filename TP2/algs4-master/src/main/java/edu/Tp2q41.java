ddc48937a79d7f5f441966263d670b5b41426462
jacob643 <jacobdorais25@hotmail.com>
2018-09-24 09:27:27 -0400

    ajout fichier test

---
+++ TP2/algs4-master/src/main/java/edu/Tp2q41.java
@@ -0,0 +1,19 @@
+package edu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class Tp2q41 {
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
