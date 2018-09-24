6d97a6ecef3afcb55211cf590d1abf12ffff43f7
jacob643 <jacobdorais25@hotmail.com>
2018-09-23 22:16:26 -0400

    ajout du test sharedbox tojson

--- Projet-TP1/TP1-code/test/SharedBoxHelperSpec.js
+++ Projet-TP1/TP1-code/test/SharedBoxHelperSpec.js
@@ -4,38 +4,38 @@ let expect = require('chai').expect;
 
 export default describe('SharedBoxHelper', () => {
   let sharedbox;
+  let sample = {
+    userEmail: 'user@acme.com',
+    guid: '1c820789a50747df8746aa5d71922a3f',
+    uploadUrl: 'upload_url',
+    recipients: [ /* list of Recipient objects*/ ],
+    attachments: [ /*list of Attachment objects*/ ],
+    message: 'lorem ipsum...',
+    subject: 'Donec rutrum congue leo eget malesuada.',
+    notificationLanguage: 'en',
+    securityOptions: {
+      allowRememberMe: true,
+      allowSms: true,
+      allowVoice: true,
+      allowEmail: true,
+      expirationValue: 5,
+      expirationUnit: 'days',
+      retentionPeriodType: 'do_not_discard',
+      retentionPeriodValue: null,
+      retentionPeriodUnit: 'hours',
+      allowManualClose: true
+    },
+    userId: 1,
+    status: 'in_progress',
+    previewUrl: 'http://sharedbox.com/sharedboxes/dhjewg67ewtfg476/preview',
+    createdAt: '2018-05-24T14:45:35.062Z',
+    updatedAt: '2018-05-24T14:45:35.589Z',
+    expiration: '2018-05-31T14:45:35.038Z',
+    closedAt: null
+  };
 
   describe('constructor', () => {
     it('should test every property of the object', () => {
-      let sample = {
-        userEmail: 'user@acme.com',
-        guid: '1c820789a50747df8746aa5d71922a3f',
-        uploadUrl: 'upload_url',
-        recipients: [ /* list of Recipient objects*/ ],
-        attachments: [ /*list of Attachment objects*/ ],
-        message: 'lorem ipsum...',
-        subject: 'Donec rutrum congue leo eget malesuada.',
-        notificationLanguage: 'en',
-        securityOptions: {
-          allowRememberMe: true,
-          allowSms: true,
-          allowVoice: true,
-          allowEmail: true,
-          expirationValue: 5,
-          expirationUnit: 'days',
-          retentionPeriodType: 'do_not_discard',
-          retentionPeriodValue: null,
-          retentionPeriodUnit: 'hours',
-          allowManualClose: true
-        },
-        userId: 1,
-        status: 'in_progress',
-        previewUrl: 'http://sharedbox.com/sharedboxes/dhjewg67ewtfg476/preview',
-        createdAt: '2018-05-24T14:45:35.062Z',
-        updatedAt: '2018-05-24T14:45:35.589Z',
-        expiration: '2018-05-31T14:45:35.038Z',
-        closedAt: null
-      };
       sharedbox = new SharedBox.Helpers.Sharedbox(sample);
       expect(sharedbox.userId).to.equal(sample.userId);
       expect(sharedbox.status).to.equal(sample.status);
@@ -61,6 +61,46 @@ export default describe('SharedBoxHelper', () => {
       expect(sharedbox.securityOptions.retentionPeriodValue).to.equal(sample.securityOptions.retentionPeriodValue);
       expect(sharedbox.securityOptions.retentionPeriodUnit).to.equal(sample.securityOptions.retentionPeriodUnit);
       expect(sharedbox.securityOptions.allowManualClose).to.equal(sample.securityOptions.allowManualClose);
+    });
+  });
+  describe('toJson', () => {
+    it('should return a JSON string of the object with proper parameters', () => {
+
+      sharedbox = new SharedBox.Helpers.Sharedbox(sample);
+      let json = sharedbox.toJson();
+      expect(json).to.be.a('string');
+      let obj = JSON.parse(json);
+      expect(obj.sharedbox.guid).to.equals(sample.guid);
+      expect(obj.sharedbox.userEmail).to.equals(sample.userEmail);
+      expect(obj.sharedbox.uploadUrl).to.equals(sample.uploadUrl);
+      expect(obj.sharedbox.subject).to.equals(sample.subject);
+      expect(obj.sharedbox.message).to.equals(sample.message);
+      expect(obj.sharedbox.userId).to.equal(sample.userId);
+      expect(obj.sharedbox.status).to.equal(sample.status);
+      expect(obj.sharedbox.previewUrl).to.equal(sample.previewUrl);
+      expect(obj.sharedbox.createdAt).to.equal(sample.createdAt);
+      expect(obj.sharedbox.updatedAt).to.equal(sample.updatedAt);
+      expect(obj.sharedbox.expiration).to.equal(sample.expiration);
+      expect(obj.sharedbox.closedAt).to.equal(sample.closedAt);
+      expect(obj.sharedbox.userEmail).to.equal(sample.userEmail);
+      expect(obj.sharedbox.uploadUrl).to.equal(sample.uploadUrl);
+      expect(obj.sharedbox.recipients).to.be.deep.equal(sample.recipients);
+      expect(obj.sharedbox.attachments).to.be.deep.equal(sample.attachments);
+      expect(obj.sharedbox.message).to.equal(sample.message);
+      expect(obj.sharedbox.subject).to.equal(sample.subject);
+      expect(obj.sharedbox.notificationLanguage).to.equal(sample.notificationLanguage);
+      expect(obj.sharedbox.securityOptions.allowRememberMe).to.equal(sample.securityOptions.allowRememberMe);
+      expect(obj.sharedbox.securityOptions.allowSms).to.equal(sample.securityOptions.allowSms);
+      expect(obj.sharedbox.securityOptions.allowVoice).to.equal(sample.securityOptions.allowVoice);
+      expect(obj.sharedbox.securityOptions.allowEmail).to.equal(sample.securityOptions.allowEmail);
+      expect(obj.sharedbox.securityOptions.expirationValue).to.equal(sample.securityOptions.expirationValue);
+      expect(obj.sharedbox.securityOptions.expirationUnit).to.equal(sample.securityOptions.expirationUnit);
+      expect(obj.sharedbox.securityOptions.retentionPeriodType).to.equal(sample.securityOptions.retentionPeriodType);
+      expect(obj.sharedbox.securityOptions.retentionPeriodValue).to.equal(sample.securityOptions.retentionPeriodValue);
+      expect(obj.sharedbox.securityOptions.retentionPeriodUnit).to.equal(sample.securityOptions.retentionPeriodUnit);
+      expect(obj.sharedbox.securityOptions.allowManualClose).to.equal(sample.securityOptions.allowManualClose);
+
+
     });
   });
 });
