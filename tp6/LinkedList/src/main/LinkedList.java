06367f50ecc11798ea3a0c4a12f4dbe8f87fe459
Billy <billy.bouchard03@gmail.com>
2018-12-03 09:25:51 -0500

    garbage

--- tp6/LinkedList/src/main/LinkedList.java
+++ tp6/LinkedList/src/main/LinkedList.java
@@ -1,77 +1,105 @@
 /**
  * @class			: LinkedList
  * @interface		: LinkedListInterface
- * @author	 		: Ons mlouki
- * 					  Ons.mlouki@gmail.com
+ * @author	                : Ons mlouki
+ *                                        Ons.mlouki@gmail.com
  */
 package main;
 
+import java.util.Comparator;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class LinkedList implements LinkedListInterface {
+public class LinkedList implements LinkedListInterface
+{
 
-	public MyListInterface build(Operator op, ArrayList<Object> val1, ArrayList<Object> val2) throws IOException{
+  public MyListInterface build(Operator op, ArrayList<Object> val1, ArrayList<Object> val2, Boolean isAscending)
+  throws IOException
+  {
+    StringBuilder chaineContent = new StringBuilder();
 
+    MyListInterface list = new MyList();
 
-		StringBuilder chaineContent=new StringBuilder();
+    list.add(val1);
+    chaineContent.append("" + val1);
 
-		MyListInterface list = new MyList();	
+    list.add(val2);
+    chaineContent.append(" " + val2);
 
-		list.add(val1);
-		chaineContent.append("" + val1);
+    SetCalculatorInterface myCalculator = new SetCalculator();
 
-		list.add(val2);
-		chaineContent.append(" " + val2);
+    int i = 1;
 
-		SetCalculatorInterface myCalculator= new SetCalculator();
+    switch (op) {
 
-		int i=1;
+    case UNION : {
+      list.add(myCalculator.union(list.getAt(i - 1), list.getAt(i)));
+      i++;
+      chaineContent.append(" " + list.getAt(i));
+    }
+      ;
+      break;
 
-		switch (op){
+    case INTERSECTION : {
+      list.add(myCalculator.intersection(list.getAt(i - 1), list.getAt(i)));
+      i++;
+      chaineContent.append(" " + list.getAt(i));
+    }
+      ;
+      break;
 
-		case UNION:{
-			list.add(myCalculator.union(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i)); 
-		}; break;
+    case DIFFERENCE : {
+      list.add(myCalculator.difference(list.getAt(i - 1), list.getAt(i)));
+      i++;
+      chaineContent.append(" " + list.getAt(i));
+    }
+      ;
+      break;
 
-		case INTERSECTION:{
-			list.add(myCalculator.intersection(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i)); 
-		}; break;
+    case SYMMETRIC_DIFFERENCE : {
+      list.add(myCalculator.symDifference(list.getAt(i - 1), list.getAt(i)));
+      i++;
+      chaineContent.append(" " + list.getAt(i));
+    }
+      ;
+      break;
 
-		case DIFFERENCE:{
-			list.add(myCalculator.difference(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i)); 
-		}; break;
+    default :
+      System.out.println(
+        "Unindentified operator " + op + ". Refer to «Operator.java» for the complete list of available operators");
+      break;
+    }
 
-		case SYMMETRIC_DIFFERENCE:{
-			list.add(myCalculator.symDifference(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i));
-		}; break;
+    return list;
+  }
 
-		case IS_SUBSET:{
-			list.add(myCalculator.isSubset(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i)); 
-
-		}; break; 
-
-		case IS_SUPERSET: {
-			list.add(myCalculator.isSuperset(list.getAt(i-1), list.getAt(i)));
-			i++;
-			chaineContent.append(" " + list.getAt(i));
-		}; break;
-
-		default:
-			System.out.println("Unindentified operator " + op + ". Refer to «Operator.java» for the complete list of available operators");
-			break;
-		}
-
-		return list;
-	}
+  public ArrayList<Object> sort(ArrayList<Object> list, boolean isAscending)
+  {
+    Comparator<Object> c;
+    if (isAscending) {
+      c = new Comparator<Object>()
+      {
+        public int compare(Object a, Object b)
+        {
+          if (a.hashCode() == b.hashCode()) {
+            return 0;
+          }
+          return a.hashCode() < b.hashCode() ? 1 : -1;
+        }
+      };
+    } else {
+      c = new Comparator<Object>()
+      {
+        public int compare(Object a, Object b)
+        {
+          if (a.hashCode() == b.hashCode()) {
+            return 0;
+          }
+          return a.hashCode() < b.hashCode() ? -1 : 1;
+        }
+      };
+    }
+    list.sort(c);
+    return list;
+  }
 }
--- tp6/LinkedList/src/main/Main.java
+++ tp6/LinkedList/src/main/Main.java
@@ -2,38 +2,40 @@ package main;
 
 import java.util.ArrayList;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception
+  {
 
-		////////////////////// Build parameters /////////////////////	
-		ArrayList<Object> val1 = new ArrayList<Object>();
-		ArrayList<Object> val2 = new ArrayList<Object>();
+    ////////////////////// Build parameters /////////////////////
+    ArrayList<Object> val1 = new ArrayList<Object>();
+    ArrayList<Object> val2 = new ArrayList<Object>();
 
-		val1.add(4);
-		val1.add(1);
-		val1.add(1);
-		val1.add(2);
-		
-		val2.add(1);
-		val2.add(1);
-		val2.add(5);
-		val2.add(5);
-		
-		//////////////////////// Linked List ///////////////////////	
-		MyListInterface list = new MyList();	
-		LinkedListInterface suiteChainee = new LinkedList();
+    val1.add(4);
+    val1.add(1);
+    val1.add(1);
+    val1.add(2);
 
-		list = suiteChainee.build(Operator.UNION, val1, val2);
-		
-        //////////////////////// Display Result ///////////////////////	
-		StringBuilder stringContent = new StringBuilder();
-		
-		for(int i = 0; i < list.getSize(); i++){
-			stringContent.append(" " + list.getAt(i));
-		}	
-		
-		System.out.println("My List : " + stringContent);
-	}
+    val2.add(1);
+    val2.add(1);
+    val2.add(5);
+    val2.add(5);
+
+    //////////////////////// Linked List ///////////////////////
+    MyListInterface list = new MyList();
+    LinkedListInterface suiteChainee = new LinkedList();
+
+    list = suiteChainee.build(Operator.UNION, val1, val2, true);
+
+    //////////////////////// Display Result ///////////////////////
+    StringBuilder stringContent = new StringBuilder();
+
+    for (int i = 0; i < list.getSize(); i++) {
+      stringContent.append(" " + list.getAt(i));
+    }
+
+    System.out.println("My List : " + stringContent);
+  }
 
 }
